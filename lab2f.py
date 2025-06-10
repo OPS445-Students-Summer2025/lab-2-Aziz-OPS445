5422569d531c120a18ff324833d88c1c8c11be14
Aziz Imam <akelifa-haji-imam@myseneca.ca>
2025-06-10 00:20:52 -0400

    Completed Lab 2

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Your Full Name
+# Author ID: yoursenecaid
+# Date Created: 2025/06/10
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
